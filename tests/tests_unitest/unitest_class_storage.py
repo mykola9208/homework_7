41885be66233f8888d27904dfc42afb4bab4136a
mykola9208 <117111441+mykola9208@users.noreply.github.com>
2022-12-23 04:47:20 +0300

    homework_7

--- tests/tests_unitest/unitest_class_storage.py
+++ tests/tests_unitest/unitest_class_storage.py
@@ -1,8 +1,31 @@
 import unittest
 from things_to_test_hw import Storage
 
-class TestClass(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls) -> None:
-        Storage.add_table('string', str)
 
+class TestClass(unittest.TestCase):
+
+    def test_positive(self):
+        string = Storage()
+        string.add_table('string', str)
+        self.assertEqual(string.get_from_table('string'), [])
+        string.add_to_table('string', 'a', 'b', 'c')
+        self.assertEqual(string.get_from_table('string'), ['a', 'b', 'c'])
+
+    def test_negative(self):
+        string = Storage()
+        string.add_table('string', str)
+        with self.assertRaises(ValueError) as exc:
+            string.add_table('string', str)
+        self.assertEqual(exc.exception.args, ('cannot override table',))
+
+        with self.assertRaises(ValueError) as exc:
+            string.add_to_table('integer', 5)
+        self.assertEqual(exc.exception.args, ('no such a table',))
+
+        with self.assertRaises(ValueError) as exc:
+            string.get_from_table('integer')
+        self.assertEqual(exc.exception.args, ('no such a table',))
+
+        with self.assertRaises(ValueError) as exc:
+            string.add_to_table('string', 5)
+        self.assertEqual(exc.exception.args, ('invalid data',))
