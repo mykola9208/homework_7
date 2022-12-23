46bb9500ada07767440c6009ffba33a9357c721b
mykola9208 <117111441+mykola9208@users.noreply.github.com>
2022-12-23 06:40:42 +0300

    homework_7

--- tests/tests_pytest/test_pytest_class_storage.py
+++ tests/tests_pytest/test_pytest_class_storage.py
@@ -2,12 +2,17 @@ import pytest
 from things_to_test_hw import Storage
 string = Storage()
 string.add_table('string', str)
+integer = Storage()
+integer.add_table('integer', int)
 
 
 def test_positive():
     assert string.get_from_table('string') == []
+    assert integer.get_from_table('integer') == []
     string.add_to_table('string', 'a', 'b', 'c')
+    integer.add_to_table('integer', 5, 6, 78)
     assert string.get_from_table('string') == ['a', 'b', 'c']
+    assert integer.get_from_table('integer') == [5, 6, 78]
 
 
 def test_negative():
@@ -25,4 +30,8 @@ def test_negative():
 
     with pytest.raises(ValueError) as exc:
         string.add_to_table('string', 5)
+    assert 'invalid data' in str(exc.value)
+
+    with pytest.raises(ValueError) as exc:
+        integer.add_to_table('integer', 5.5)
     assert 'invalid data' in str(exc.value)
